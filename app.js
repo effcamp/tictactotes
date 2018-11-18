dc62f5ecb9910f3fcd57be8868ab8400276a5ce2
Fred Campos <fcf.camposx@gmail.com>
2018-11-17 23:15:52 -0400

    Clicking adds Xs to board

--- app.js
+++ app.js
@@ -1,10 +1,56 @@
-const x = document.createElement('h1');
-x.innerText = 'X';
-const o = document.createElement('h1');
-o.innerText = 'O';
-x.className += 'big';
-o.className += 'big';
-const placeMove = e => {
-  e.appendChild(x);
-  // console.log(e.target);
+// window.onload = () => {
+//   // while (true) {
+//   //   console.log(1);
+//   // }
+// };
+
+const board = new Array(9).fill(null);
+let boardDiv = [...document.querySelectorAll('.board')];
+
+const startGame = e => {
+  e.preventDefault();
+  let user = 1;
+  document.getElementById('player').innerText = document.getElementById(
+    'name'
+  ).value;
+
+  // console.log(boardDiv);
+  boardDiv.forEach(b => {
+    b.addEventListener('click', e => placeMove(e, user));
+  });
 };
+const placeMove = (e, user) => {
+  // console.log(user);
+  let shape;
+  shape = document.createElement('h1');
+  shape.className += 'big';
+
+  if (user === 1) {
+    shape.innerText = 'X';
+  } else {
+    shape.innerText = 'O';
+  }
+  const divId = e.target.id;
+  if (!board[divId]) {
+    board[divId] = shape;
+    boardDiv[divId].appendChild(board[divId]);
+  } else {
+    console.log('Invalid move');
+  }
+
+  // e.target.appendChild(x);
+  // console.log(e.target.id);
+  // let div = e.target.id;
+
+  // board[div] = x;
+  // console.log(board);
+  // board.forEach((el, i) => {
+  //   if (el) {
+  //     document.getElementById(i).appendChild(el);
+  //   } else {
+  //     // console.log('invalid');
+  //   }
+  // });
+};
+
+const win = () => true;
