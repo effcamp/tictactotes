ffb736cc5163fb04473fd652c4a61d2aae6c8410
Fred Campos <fcf.camposx@gmail.com>
2018-11-19 17:37:19 -0400

    Added NPM, dotenv, and parcel

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-//test
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
 
-const db = firebase.firestore();
-db.settings({
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Initialize Firebase
+var config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+const db = firebase.initializeApp(config);
+
+db.firestore().settings({
   timestampsInSnapshots: true
 });
 
--- env.js
+++
@@ -1,8 +0,0 @@
-const API = {
-  apiKey: null,
-  authDomain: null,
-  databaseURL: null,
-  projectId: null,
-  storageBucket:null,
-  messagingSenderId: null
-}
